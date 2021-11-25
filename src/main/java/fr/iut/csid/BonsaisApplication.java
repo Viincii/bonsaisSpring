b246f905a73184715d0ec185016a90ed91257fdb
vmignot <vinc.mignot@hotmail.fr>
2021-11-25 14:20:53 +0100

    [FIX] SSH

--- src/main/java/fr/iut/csid/BonsaisApplication.java
+++ src/main/java/fr/iut/csid/BonsaisApplication.java
@@ -20,4 +20,6 @@ public class BonsaisApplication {
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build();
     }
+
+    //SSH
 }
